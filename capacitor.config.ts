54e02250f6e350dcd4bbb6a3b42c517050f281ae
Cate <caterinadefranchis@epet12smandes.edu.ar>
2025-09-19 13:55:49 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bookish',
+  webDir: 'dist'
+};
+
+export default config;
