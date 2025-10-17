1d3ef0af70ba97a1d2f189e76efd12c107088239
caterina-tais <caterinadefranchis@epet12smandes.edu.ar>
2025-10-17 10:41:38 -0300

    estilps tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,11 @@
 // src/pages/Tab1.tsx
-import { IonContent, IonPage } from "@ionic/react";
+import { IonContent, IonPage, IonIcon } from "@ionic/react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "../components/loginButton";
 import { LogoutButton } from "../components/logoutButton";
 import { Profile } from "../components/profile";
+import { bookOutline } from 'ionicons/icons';
+import './Tab1.css';
 
 const Tab1: React.FC = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -12,15 +14,21 @@ const Tab1: React.FC = () => {
 
   return (
     <IonPage>
-      <IonContent className="ion-padding">
-        {!isAuthenticated ? (
-          <LoginButton />
-        ) : (
-          <>
-            <Profile />
-            <LogoutButton />
-          </>
-        )}
+      <IonContent>
+        <div className="welcome-container">
+          <h1 className="welcome-title">¡Bienvenido!</h1>
+          <IonIcon icon={bookOutline} className="book-icon" />
+          <div className="auth-container">
+            {!isAuthenticated ? (
+              <LoginButton />
+            ) : (
+              <>
+                <Profile />
+                <LogoutButton />
+              </>
+            )}
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
